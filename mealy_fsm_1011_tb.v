405cd2df7b1854e57b77bf559a6b60eeb13e3112
NITHISHWAR P <nithishwar2709@gmail.com>
2025-10-12 15:02:54 +0530

    Add files via upload

---
+++ mealy_fsm_1011_tb.v
@@ -0,0 +1,30 @@
+module mealy_fsm_1011_tb;
+        reg clk_t,rst_t,xin_t;
+        wire zout_t;
+        
+        mealy_fsm_1011 dut(.clk(clk_t),.rst(rst_t),.xin(xin_t),.zout(zout_t));
+           
+     initial
+        begin
+            clk_t = 1'b1;
+            rst_t = 1'b1;
+          #100
+            rst_t = 1'b0;
+            xin_t = 1'b1;
+          #100
+            xin_t = 1'b0;
+          #100
+            xin_t = 1'b1;
+          #100
+            xin_t = 1'b1;
+          #100
+            xin_t = 1'b1;
+          #100
+            xin_t = 1'b0;
+          #100
+            xin_t = 1'b1; 
+          #100
+           xin_t = 1'b1;              
+      end
+       always #50  clk_t = ~clk_t;                 
+endmodule
